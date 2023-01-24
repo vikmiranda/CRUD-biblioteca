7713ef3d95f8e9e5f37b9ed28764f68d0fe593fb
vikmiranda <jvkmiranda@hotmail.com>
2023-01-24 15:16:00 -0300

    fix html: bug divisao dos livros

--- includes/ver_livros.php
+++ includes/ver_livros.php
@@ -39,10 +39,9 @@
                                         <a href="excluir.php?id='.$livro->ID_livro.'" >
                                         <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Excluir</button>
                                         </a>
-
-
-
-
+                                        </div>
+                    </div>
+                    </div>
                     </div>';
  
     }
